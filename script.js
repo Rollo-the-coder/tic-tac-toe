f670e1765d2465281e82b35633e982d6e7d5151e
Rollo-the-coder <erikgotta@gmail.com>
2020-12-01 22:57:17 +0700

    Start of factories and modules for tic-tac-toe

---
+++ script.js
@@ -0,0 +1,40 @@
+//playerFactory
+const player = (name) => {
+    const welcome = () => console.log('Welcome!');
+    return {name, welcome};
+}
+
+const player1 = player('Erik');
+const player2 = player('Anh');
+
+//gameboardModule pattern
+const gameBoard = (() => {
+    const array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    const board = document.getElementsByTagName("playarea");
+    //need to make choice below connect somehow with gameflow, do with prototype
+    document.getElementById("block0").addEventListener("click", choice)
+    return {
+        array,
+        board,
+    }
+})();
+
+//gameFlowModule pattern
+const gameFlow = (() => {
+
+//loop for turns, checking turns based on even and odd
+for (i = 0; i < gameBoard.array.length; i++) {
+    if(i % 2 == 0) {
+
+    }
+    else {
+
+    }
+}
+
+
+
+})();
+
+
+
