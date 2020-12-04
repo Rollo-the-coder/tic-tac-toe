e0ef99e743363f07e433c1216df0736746cdbb07
Rollo-the-coder <erikgotta@gmail.com>
2020-12-05 00:21:03 +0700

    trying to understand how to connect everything

--- script.js
+++ script.js
@@ -7,34 +7,33 @@ const player = (name) => {
 const player1 = player('Erik');
 const player2 = player('Anh');
 
+//gameFlowModule pattern
+const gameFlow = (() => {
+
+    //loop for turns, checking turns based on even and odd
+    for (i = 0; i < gameBoard.array.length; i++) {
+        if(i % 2 == 0) {
+            playarea.value = "X";
+        }
+        else {
+            playarea.value = "O";
+        }
+    }
+    })();
+
 //gameboardModule pattern
 const gameBoard = (() => {
     const array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     const board = document.getElementsByTagName("playarea");
     //need to make choice below connect somehow with gameflow, do with prototype
-    document.getElementById("block0").addEventListener("click", choice)
+    document.getElementById("block0").addEventListener("click", gameFlow)
     return {
         array,
         board,
     }
 })();
 
-//gameFlowModule pattern
-const gameFlow = (() => {
 
-//loop for turns, checking turns based on even and odd
-for (i = 0; i < gameBoard.array.length; i++) {
-    if(i % 2 == 0) {
-
-    }
-    else {
-
-    }
-}
-
-
-
-})();
 
 
 
