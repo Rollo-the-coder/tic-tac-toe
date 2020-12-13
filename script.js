88b9e85ca368e830c54cde9d109833b524b6dae3
Rollo-the-coder <erikgotta@gmail.com>
2020-12-13 18:02:19 +0700

    trying to create player names in factory function

--- script.js
+++ script.js
@@ -1,37 +1,42 @@
 //playerFactory
 const player = (name) => {
+    const name = document.querySelector("#names").addEventListener("input");
+    // const playerName = () => {
+    //     playName = document.querySelector("#names").innerHTML 
+    //     return {playName};    
+    // }
     const welcome = () => console.log('Welcome!');
-    return {name, welcome};
+    return {name, welcome,};
 }
 
-const player1 = player('Erik');
-const player2 = player('Anh');
+const playerX = player(theName);
+const playerO = player(theName);
 
 //gameFlowModule pattern
-const gameFlow = (() => {
+// const gameFlow = (() => {
 
-    //loop for turns, checking turns based on even and odd
-    for (i = 0; i < gameBoard.array.length; i++) {
-        if(i % 2 == 0) {
-            playarea.value = "X";
-        }
-        else {
-            playarea.value = "O";
-        }
-    }
-    })();
+//     //loop for turns, checking turns based on even and odd
+//     for (i = 0; i < gameBoard.array.length; i++) {
+//         if(i % 2 == 0) {
+//             gameBoard.board.value = "X";
+//         }
+//         else {
+//             gameBoard.board.value = "O";
+//         }
+//     }
+//     })();
 
-//gameboardModule pattern
-const gameBoard = (() => {
-    const array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    const board = document.getElementsByTagName("playarea");
-    //need to make choice below connect somehow with gameflow, do with prototype
-    document.getElementById("block0").addEventListener("click", gameFlow)
-    return {
-        array,
-        board,
-    }
-})();
+// //gameboardModule pattern
+// const gameBoard = (() => {
+//     const array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+//     const board = document.getElementsByTagName("playarea");
+//     //need to make choice below connect somehow with gameflow, do with prototype
+//     document.getElementById("block0").addEventListener("click", gameFlow)
+//     return {
+//         array,
+//         board,
+//     }
+// })();
 
 
 
